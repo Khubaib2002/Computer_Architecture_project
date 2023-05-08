d95d6a14aba92a797ff3cd0855b102e70103e501
Owais Aijaz <owaisaijaz2003@gmail.com>
2023-05-08 16:02:06 +0500

    Task 3 Final

--- Task3_HazardDetection/Lab_11.srcs/sources_1/new/MUX.v
+++ Task3_HazardDetection/Lab_11.srcs/sources_1/new/MUX.v
@@ -1,26 +1,74 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 03/28/2023 11:19:10 AM
-// Design Name: 
-// Module Name: MUX
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+module Mux
+(
+	input [63:0] a,
+	input [63:0] b,
+	input  sel,
+	
+	output reg [63:0]data_out
+);
+
+//assign data_out = sel? b:a ;
+
+always @ (a, b, sel)
+begin
+	if (!sel)
+		data_out = a;
+	else
+		data_out = b;
+		
+end
+
+endmodule 
 
 
-module MUX(
 
-    );
-endmodule
+
+module Mux2
+(
+	input [63:0] a,
+	input [63:0] b,
+	input  sel,
+	
+	output reg [63:0]data_out
+);
+
+//assign data_out = sel? b:a ;
+
+always @ (a, b, sel)
+begin
+	case (sel)
+	1'b0: data_out = a;
+	1'b1: data_out = b;
+	1'bx: data_out = a;
+	endcase
+		
+end
+
+endmodule 
+
+
+
+module Mux3
+(
+	input [63:0] a,
+	input [63:0] b,
+	input [63:0] c,
+	input [1:0] sel,
+	
+	output reg [63:0] data_out
+);
+
+//assign data_out = sel? b:a ;
+
+always @ (a, b, c, sel)
+begin
+	case (sel)
+	2'b00: data_out = a;
+	2'b01: data_out = b;
+	2'b10: data_out = c;
+	1'bx: data_out = a;
+	endcase
+		
+end
+
+endmodule 
